01724ddc9ccf4bdb48c1188ed8e9bc6540c475f3
Daniel Soares <danielandradesoares@hotmail.com>
2021-11-04 14:16:36 -0300

    Fixed

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import Head from  'next/head'
-
-import Layout from '../components/Layout/Layout'
+import Layout from '../components/layout/layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
